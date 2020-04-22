c6637450c339d373fac099960d87d304ae6e0691
aitorllanos99 <UO264476@gmail.com>
2020-04-22 11:26:25 +0200

    “SDI-SW-11.1 Servicio SOAP”.

---
+++ sdi1920-508-lab-soap-sw/src/test/java/com/uniovi/Sdi1920508LabSoapSwApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uniovi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Sdi1920508LabSoapSwApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
